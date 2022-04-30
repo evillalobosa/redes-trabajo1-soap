caff4e650714f5a2450d805e4c1c59071fc3b2e7
matalvarez10 <matias.alvarezc@utem.cl>
2022-04-29 21:10:29 -0400

    adding the new server

---
+++ server/WebApplication3/WebService1.asmx.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace WebApplication3
+{
+    /// <summary>
+    /// Summary description for WebService1
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WebService1 : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+        [WebMethod]
+        public int addition(int a, int b)
+        {
+            return (a + b);
+        }
+        [WebMethod]
+        public int verificaRut(int rut, string dv)
+        {
+            int Digito;
+            int Contador;
+            int Multiplo;
+            int Acumulador;
+            string RutDigito;
+
+            Contador = 2;
+            Acumulador = 0;
+
+            while (rut != 0)
+            {
+                Multiplo = (rut % 10) * Contador;
+                Acumulador = Acumulador + Multiplo;
+                rut = rut / 10;
+                Contador = Contador + 1;
+
+                if (Contador == 8)
+                {
+                    Contador = 2;
+                }
+            }
+
+
+            Digito = 11 - (Acumulador % 11);
+            RutDigito = Digito.ToString().Trim();
+            if (Digito == 10)
+            {
+                RutDigito = "K";
+            }
+            if (Digito == 11)
+            {
+                RutDigito = "0";
+            }
+
+
+            if (RutDigito.ToString() == dv.ToString())
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+
+        }
+
+    }
+}
